67e945f1deba09cdec64e0e99a259a71ae40fe0f
Otrix98 <Otrix98@gmail.com>
2020-05-30 01:15:53 +0300

    10.1

--- app/src/main/java/com/example/module_61/MainFragment.kt
+++ app/src/main/java/com/example/module_61/MainFragment.kt
@@ -1,14 +1,14 @@
 package com.example.module_61
 
+
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.view.children
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_login.*
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainFragment: Fragment(R.layout.fragment_main), ItemSelectListener {
 
@@ -29,12 +29,14 @@ class MainFragment: Fragment(R.layout.fragment_main), ItemSelectListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
+        val tabletSize = resources.getBoolean(R.bool.isTablet)
+        if (!tabletSize) {
         childFragmentManager.beginTransaction()
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .add(R.id.container_main, ListFragment())
-//            .addToBackStack(null)
+            .addToBackStack(null)
             .commit()
-
+    }
 
     }
 //    fun addDetail() {
@@ -54,6 +56,7 @@ class MainFragment: Fragment(R.layout.fragment_main), ItemSelectListener {
 
     override fun onItemSelected(text: String) {
         childFragmentManager.beginTransaction()
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
             .add(R.id.container_main, DetailFragment.newInstance(text))
             .addToBackStack(null)
             .commit()
